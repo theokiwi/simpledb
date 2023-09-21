260b7a99abc2a9a30739004827740941825f2d7a
Theo DIniz Viana <65356457+theokiwi@users.noreply.github.com>
2023-09-21 09:44:13 -0300

    Build 2 - Criação Debug Tools, Inicio dbRemove e dbSearch
    - É Possivel printar todo o dicionario/map
    - O dbRemove ta pronto só falta resolver como eu vou pesquisar pelo dicionario e descobrir se o valor ta la pra ser removido
    - Falta implementar o dbSearch

--- main.cpp
+++ main.cpp
@@ -17,8 +17,18 @@ class getUserInput {
       std::cin >> userValue;
 
       return userValue;
-  }
+  } 
   
+};
+
+class debugTools{
+  public:
+
+  void displayDB(std::map<std::string, std::string> db){
+    for (auto pair : db){
+      std::cout << pair.first << " " << pair.second << std::endl;
+    }
+  }
 };
 
 class simpledb{
@@ -27,23 +37,41 @@ class simpledb{
   std::map<std::string, std::string> db; 
 
   void dbInsert(){
-    std::string userValue = input.getValue();
     std::string userKey = input.getKey();
+    std::string userValue = input.getValue();
 
     db.insert(std::pair<std::string, std::string>(userKey, userValue));
     std::cout << "a chave é " << userKey << " o valor é " << userValue << std::endl;
   }
+
+  bool dbSearch(std::map<std::string, std::string> db, std::string userValue){
+    std::string value = userValue;
+    std::string* ptrValue;
+    for(int i = 0; i < sizeof(db)/sizeof(std::string); i++){
+      //loop pelo dicionario e comparar valor, retornar true se achar;
+    }
+  }
+
+  void dbRemove(std::map<std::string, std::string> db, std::string userValue){
+  if(dbSearch(db, userValue)){
+    std::string userKey = input.getKey();
+    db.erase((userKey));
+    std::cout << "a chave removida foi " << userKey;
+  }
+
 };
 
 int main()
 {
   getUserInput input;
   simpledb db;
+  debugTools debug;
 
   db.dbInsert();
+  debug.displayDB(db.db);
   return 0;
 }
 
 
 
-
+};
